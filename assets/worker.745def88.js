dc64f9ff0f24816292a445d274b71feb8410e4de
zhong_wei_peng <2783335126@qq.com>
2023-10-15 10:42:00 +0800

    更新球队批量更新

--- assets/worker.745def88.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";addEventListener("message",(e=>{const{data:t}=e;let s=[];return t.forEach(((e,a)=>{if(a>0){let a={};e.forEach(((e,s)=>{a[t[0][s]]=e})),s.push(a)}})),postMessage(s)}))}();
